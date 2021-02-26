98fb1066f0e6b0f7310d37252b4dc4c6a7e195bd
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-02-26 10:09:16 +0530

    ADD. added new file emp wage calculate total working hours

---
+++ UC-6emWageCalToatalWorkingHours.sh
@@ -0,0 +1,69 @@
+#!/bin/bash -x
+
+#CONSTANTS
+isPartTime=1;
+isFullTime=2;
+empWagePerHour=20;
+workingDaysAMonth=20;
+maxHoursAMonth=100;
+totalEmpHours=0;
+#VARIABLES
+#numWorkingDays=20;
+totalWorkingDays=0;
+
+
+while [[ $totalEmpHours -lt $maxHoursAMonth && $totalWorkingDays -lt $workingDaysAMonth ]]
+do
+                ((totalWorkingDays++))
+                empCheck=$((RANDOM%3));
+                case $empCheck in
+                        $isFullTime)
+                                numberofWorkingHours=8
+                                ;;
+                        $isPartTime)
+                                numberofWorkingHours=4
+                                ;;
+                        *)
+                                numberofWorkingHours=0
+                                ;;                                                                                                                                                                                                                                                                                                                              esac
+                (( totalEmpHours+=numberofWorkingHours ))
+done
+
+totalSalary=$(( totalEmpHours * empWagePerHour ))
+echo "total salary is $totalSalary ";
+
+#!/bin/bash -x
+
+#CONSTANTS
+isPartTime=1;
+isFullTime=2;
+empWagePerHour=20;
+workingDaysAMonth=20;
+maxHoursAMonth=100;
+totalEmpHours=0;
+#VARIABLES
+#numWorkingDays=20;
+totalWorkingDays=0;
+
+
+while [[ $totalEmpHours -lt $maxHoursAMonth && $totalWorkingDays -lt $workingDaysAMonth ]]
+do
+                ((totalWorkingDays++))
+                empCheck=$((RANDOM%3));
+                case $empCheck in
+                        $isFullTime)
+                                numberofWorkingHours=8
+                                ;;
+                        $isPartTime)
+                                numberofWorkingHours=4
+                                ;;
+                        *)
+                                numberofWorkingHours=0
+                                ;;                                                                                                                                                                                                                                                                                                                              esac
+                (( totalEmpHours+=numberofWorkingHours ))
+done
+
+totalSalary=$(( totalEmpHours * empWagePerHour ))
+echo "total salary is $totalSalary ";
+
+
